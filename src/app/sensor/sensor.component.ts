c9ba566215291e5397957ad15f61b8bc0d9e51aa
FSANCHEZH <franciscosanchezherrador@gmail.com>
2019-03-26 00:09:12 +0100

    feat: updated sensor.component

--- src/app/sensor/sensor.component.ts
+++ src/app/sensor/sensor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ISensor } from '../interfaces/sensor';
 
 @Component({
   selector: 'app-sensor',
@@ -7,30 +8,25 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 
 export class SensorComponent implements OnInit {
-  @Input() id: number;
-  @Input() latitude: number;
-  @Input() longitude: number;
-  icono: string;
-  @Input() temperatura: string;
-  @Input() bateria: string;
-  @Input() cobertura: string;
-  @Input() incidencias: number;
+  @Input() sensor: ISensor;
+  public icono: string;
   public isDisabled: boolean = false;
+
   public ngOnInit() {
     this.chooseIcon();
-    if(this.incidencias == 0){
+    if(this.sensor.incidencias == 0){
       this.isDisabled = true;
     }
   }
 
   private chooseIcon() {
-    if (this.incidencias < 1) {
+    if (this.sensor.incidencias < 1) {
       this.icono = './assets/images/signal_0_incidencia.png';
-    } else if (this.incidencias < 2) {
+    } else if (this.sensor.incidencias < 2) {
       this.icono = './assets/images/signal_1_incidencia.png';
-    } else if (this.incidencias < 3) {
+    } else if (this.sensor.incidencias < 3) {
       this.icono = './assets/images/signal_2_incidencia.png';
-    } else if (this.incidencias < 4) {
+    } else if (this.sensor.incidencias < 4) {
       this.icono = './assets/images/signal_3_incidencia.png';
     } else {
       this.icono = './assets/images/signal_3+_incidencia.png';
